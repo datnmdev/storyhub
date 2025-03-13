dfc3ab93d8c0abb6facdb61c64b64e016d1ef525
Nguyen Minh Dat <datnm.ptit@gmail.com>
2025-03-13 15:28:16 +0700

    config: Tạo và cấu hình dự án (#2) (#54)
    config: Tạo dự án ReactJS và cấu hình cho dự án (#2)
    
    
    
    config: Tạo dự án NestJS và cấu hình dự án (#2)
    
    Signed-off-by: datnmptit <datnm.ptit@gmail.com>

---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule, {
+		bufferLogs: true,
+	});
+	app.setGlobalPrefix('api/v1');
+	app.useGlobalPipes(new ValidationPipe());
+	app.enableCors({
+		origin: 'http://localhost:5173',
+	});
+	await app.listen(process.env.PORT);
+}
+bootstrap();

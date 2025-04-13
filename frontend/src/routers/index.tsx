5e732bc0b7a84e18984ad07ab03872a52c6cf2e5
datnmptit <datnm.ptit@gmail.com>
2025-04-14 00:58:05 +0700

    feat: Tạo tính năng thay đổi mật khẩu dành cho tác giả (#101)
    Signed-off-by: datnmptit <datnm.ptit@gmail.com>

--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -343,6 +343,16 @@ const router = createBrowserRouter([
         ),
       },
       {
+        path: paths.authorChangePasswordPage(),
+        element: (
+          <Protected role={Role.AUTHOR}>
+            <AuthorLayout>
+              <AuthorChangePasswordPage />
+            </AuthorLayout>
+          </Protected>
+        ),
+      },
+      {
         path: paths.moderatorHomePage(),
         element: (
           <Protected role={Role.MODERATOR}>

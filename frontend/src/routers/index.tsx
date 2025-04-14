0b01b118c656940564ec861fda298612e0b449f5
datnmptit <datnm.ptit@gmail.com>
2025-04-15 00:40:23 +0700

    feat: Tạo trang web chức năng cập nhật thông tin truyện (#17)
    Signed-off-by: datnmptit <datnm.ptit@gmail.com>

--- frontend/src/routers/index.tsx
+++ frontend/src/routers/index.tsx
@@ -293,6 +293,16 @@ const router = createBrowserRouter([
         ),
       },
       {
+        path: paths.authorUpdateStoryPage(),
+        element: (
+          <Protected role={Role.AUTHOR}>
+            <AuthorLayout>
+              <AuthorUpdateStoryPage />
+            </AuthorLayout>
+          </Protected>
+        ),
+      },
+      {
         path: paths.moderatorHomePage(),
         element: (
           <Protected role={Role.MODERATOR}>

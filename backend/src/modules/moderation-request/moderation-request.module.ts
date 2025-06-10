021b8e7fe3908da2fbec3b18d75a6ab7ae409912
Nguyen Minh Dat <datnm.ptit@gmail.com>
2025-06-10 09:28:13 +0700

    Fix/some error (#120)
    Signed-off-by: datnmptit <datnm.ptit@gmail.com>

--- backend/src/modules/moderation-request/moderation-request.module.ts
+++ backend/src/modules/moderation-request/moderation-request.module.ts
@@ -4,9 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ModerationRequest } from './entities/moderation-request';
 import { ModerationRequestService } from './moderation-request.service';
 import { UrlCipherModule } from '@/common/url-cipher/url-cipher.module';
+import { BullModule } from '@/common/bull/bull.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ModerationRequest]), UrlCipherModule],
+  imports: [
+    TypeOrmModule.forFeature([ModerationRequest]),
+    UrlCipherModule,
+    BullModule,
+  ],
   controllers: [ModerationRequestController],
   providers: [ModerationRequestService],
 })
